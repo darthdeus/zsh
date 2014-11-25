fc0d9fadbcf62fda0d2ffd0e033678a7a96ddb3a
Jakub Arnold <darthdeus@gmail.com>
2014-11-25 22:02:53 +0100

    Fix base16 colors

--- templates/zshrc.zsh
+++ templates/zshrc.zsh
@@ -55,3 +55,7 @@ fi
 
 ### Added by the Heroku Toolbelt
 export PATH="/usr/local/heroku/bin:$PATH"
+
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
+[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
