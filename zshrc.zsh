aa8281e841216b4f548b2d182bf7c53da8543cfa
Jakub Arnold <darthdeus@gmail.com>
2015-03-06 13:31:09 +0100

    Remove pow plugin from zshrc since it pollutes touch

--- zshrc.zsh
+++ zshrc.zsh
@@ -22,7 +22,7 @@ ZSH_THEME="darth"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(rbenv pow)
+plugins=(rbenv)
 
 source $ZSH/oh-my-zsh.sh
 
