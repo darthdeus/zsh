cb255ca5bc9a9bc85371920422c48e01791320d6
Jakub Arnold <darthdeus@gmail.com>
2015-09-09 09:49:53 +0000

    Remove GREP_OPTIONS in favor of an alias

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,23 +1,22 @@
 # Push and pop directories on directory stack
-alias pu='pushd'
-alias po='popd'
+alias pu="pushd"
+alias po="popd"
 
 # Basic directory operations
-alias ...='cd ../..'
-alias -- -='cd -'
+alias ...="cd ../.."
+alias -- -="cd -"
 
 # Super user
-alias _='sudo'
-
-#alias g='grep -in'
+alias _="sudo"
 
 # Show history
-alias history='fc -l 1'
+alias history="fc -l 1"
 
 # List direcory contents
-alias lsa='ls -lah'
-alias l='ls -la'
-alias ll='ls -l'
+alias lsa="ls -lah"
+alias l="ls -la"
+alias ll="ls -l"
 alias sl=ls # often screw this up
 
-alias afind='ack-grep -il'
+alias afind="ack-grep -il"
+alias grep="grep --color=auto"
